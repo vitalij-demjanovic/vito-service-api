47f49af74e386b29a1e51c5bce9cadf5b8c20def
vitalij-demjanovic <vitalijdemjanovic@gmail.com>
2023-07-25 21:59:12 +0200

    create categories module

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+	constructor(private categoriesService: CategoriesService) {}
+
+	@Post('create')
+	create(@Body() dto: CreateCategoryDto) {
+		return this.categoriesService.createCategory(dto);
+	}
+
+	@Get('/:value')
+	getByValue(@Param('value') value: string) {
+		return this.categoriesService.getCategoryByValue(value);
+	}
+}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+	constructor(
+		@InjectRepository(Category)
+		private readonly categoriesRepository: Repository<Category>,
+	) {}
+
+	async createCategory(dto: CreateCategoryDto) {
+		return this.categoriesRepository.save(dto);
+	}
+
+	async getCategoryByValue(value: string) {
+		return this.categoriesRepository.findOne({ where: { name: value } });
+	}
+}
